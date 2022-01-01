a7c32092a59ac9525700a25e48bc32f287229c00
alu0100906813 <alu0100906813@ull.edu.es>
2022-01-01 12:32:11 +0000

    Cambios en conexión desde broker a bbdd

--- src/DBSuscriptor/dbConfig.py
+++ src/DBSuscriptor/dbConfig.py
@@ -1,12 +1,19 @@
 
 {
-  'broker' : {
+  'brokers' : {
+    'kafka-1' : {
+      'host' : '',
+      'port' : ''
+    },
+    'kafka-2' : {
+      'host' : '',
+      'port' : ''
+    }
+  },
+  'database' : {
     'host' : '',
     'username' : '',
     'password' : '',
-    'bucket' : ''
-  },
-  'suscriptor' : {
-
+    'bucket' : ''     
   }
 }
