b256713e77ab504d574d48aa9a89510cde779cdd
alu0100906813 <alu0100906813@ull.edu.es>
2022-01-20 13:33:35 +0000

    Añadido el multisensor al programa de python de las raspberry pi

--- src/RaspberryPI/main.py
+++ src/RaspberryPI/main.py
@@ -11,19 +11,21 @@ config = None
 with open('config.json', 'r') as jsonFile:
   config = json.load(jsonFile)
 
-input = SenseHatController()
+inputs = [SenseHatController()]
 
 broker = Broker(config)
 
 def main():
   place = f"{config['place']}/" if 'place' in config else ''
   while True:
-    sensorValues = input.getData()
+    sensorValues = dict()
+    for input in inputs:
+      sensorValues = { **sensorValues, **input.getData() }
     for sensorName in sensorValues:
       broker.publish(
         f"{config['ship']}/{place}{sensorName}",
-        {"value" : sensorValues[sensorName], "time" : str(datetime.datetime.now())}
+        {"value" : sensorValues[sensorName], "time" : str(datetime.datetime.utcnow())}
       )
-    time.sleep(1.5) # Ralentizamos ya que hay un cuello de botella al insertar en la bbdd
+    time.sleep(1) # Ralentizamos ya que hay un cuello de botella al insertar en la bbdd
 
 main()
