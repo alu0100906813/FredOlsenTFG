e1f07df23a0c3704f243a6e0e99946b2add54574
alu0100906813 <56115508+alu0100906813@users.noreply.github.com>
2022-03-03 14:49:22 +0000

    Create MSDV.py

---
+++ src/MatLab Script/MSDV.py
@@ -0,0 +1,29 @@
+
+import numpy
+import scipy.integrate as integrate
+import math
+
+aw = input("Introduzca el valor de AW: ")
+
+try:
+  aw = float(aw)
+except:
+  print("El valor de AW tiene que ser flotante")
+  exit(-1)
+
+time = input("Introduzca el intervalo de tiempo en minutos: ")
+
+try:
+  time = int(time)
+except:
+  print("El valor del tiempo tiene que ser entero")
+  exit(-1)
+
+time = time * 60 * 10 # Pasamos a centisegundos, ya que el muestreo es de 10 datos por segundo
+
+awPowTwo = pow(aw, 2)
+integralFromZeroToTime = integrate.quad(lambda x : awPowTwo, 0, time)
+
+MSDV = math.sqrt(integralFromZeroToTime[0])
+
+print("El valor de MSDV es de ", MSDV)
